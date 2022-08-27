47958eb50f9c0700ce15f73bdbdd8ee4cf620c11
Valetina-2608 <valusha2608@gmail.com>
2022-08-27 14:22:27 +0300

    Create function addToOrder

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Script */
+
+
+let btns_add_ingredients = document.querySelectorAll('.add_ingredient');
+for(let i = 0; i < btns_add_ingredients.length; i++){
+    btns_add_ingredients[i].addEventListener('click',addToOrder);
+}
+
+function addToOrder(event){
+    let add_ingredient = event.target;
+    let add_ingredient_parent = add_ingredient.parentElement;
+    let name = add_ingredient_parent.children[1].innerHTML;
+    let price = add_ingredient_parent.children[2].children[2].innerHTML;
+    let quantity = 1;
+    let order = document.getElementById('order');
+    let new_ingredient = document.createElement('div');
+    new_ingredient.classList.add('new_ingredient');
+    new_ingredient.innerHTML = `<span class="name">${name}</span>
+    <span class="quantity">${quantity}</span>
+    <span class="price">${price}</span>
+    <button class="btn_remove">Remove</button>`
+    order.appendChild(new_ingredient);
+}
